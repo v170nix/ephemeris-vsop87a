60b029b4dfa779500240f4214b9fcaeba7511f47
Vitaliy Sheyanov <vit.onix@gmail.com>
2019-05-15 01:35:27 +0300

    version 0.8.1

--- src/test/kotlin/net/arwix/astronomy2/ephemeris/vsop87a/Vsop87EphemerisFunctionsTest.kt
+++ src/test/kotlin/net/arwix/astronomy2/ephemeris/vsop87a/Vsop87EphemerisFunctionsTest.kt
@@ -14,25 +14,25 @@ internal class Vsop87EphemerisFunctionsTest {
     @MethodSource("getData")
     fun `Coordinates`(data: Data) {
         runBlocking {
-            val vector = createSuspendedVsop87ACoordinates(data.id).invoke(this, data.jT).also {
+            val vector = createSuspendedVsop87ACoordinates(data.id).invoke(data.jT).also {
                 System.out.println(it)
             }
             assertArrayEquals(data.vector, vector.toArray())
         }
     }
 
-    internal data class Data(val id: Int, val jT: Double, val vector: DoubleArray)
+    internal data class Data(val id: VsopData, val jT: Double, val vector: DoubleArray)
 
     fun getData() = listOf(
-            Data(ID_VSOP87_MERCURY, 0.5, doubleArrayOf(-0.17951421009745386, 0.2678116610871014, 0.038348376780681856)),
-            Data(ID_VSOP87_VENUS, 0.5, doubleArrayOf(0.1417824775000777, -0.7133841642050578, -0.01802581474852584)),
-            Data(ID_VSOP87_EARTH, 0.5, doubleArrayOf(-0.1716125006836458, 0.9682587683227163, -1.0977609826272948E-4)),
-            Data(ID_VSOP87_EM_BARYCENTER, 0.5, doubleArrayOf(-0.171583296518176, 0.9682682377875933, -1.0795861692371957E-4)),
-            Data(ID_VSOP87_MARS, 0.5, doubleArrayOf(-1.5432315120596511, -0.5035899853317461, 0.027202004460763252)),
-            Data(ID_VSOP87_JUPITER, 0.5, doubleArrayOf(-2.3910546127650303, 4.664070272032234, 0.033966714165738876)),
-            Data(ID_VSOP87_SATURN, 0.5, doubleArrayOf(4.766239751052515, -8.7737132940204, -0.037879624876213304)),
-            Data(ID_VSOP87_URANUS, 0.5, doubleArrayOf(-17.823171829534186, 4.071512823576951, 0.2458509882743705)),
-            Data(ID_VSOP87_NEPTUNE, 0.5, doubleArrayOf(17.397928561207056, 24.197927112419535, -0.8992271522836394))
+            Data(Vsop87AMercuryData, 0.5, doubleArrayOf(-0.17951421009745386, 0.2678116610871014, 0.038348376780681856)),
+            Data(Vsop87AVenusData, 0.5, doubleArrayOf(0.1417824775000777, -0.7133841642050578, -0.01802581474852584)),
+            Data(Vsop87AEarthData, 0.5, doubleArrayOf(-0.1716125006836458, 0.9682587683227163, -1.0977609826272948E-4)),
+            Data(Vsop87AEarthBarycenterData, 0.5, doubleArrayOf(-0.171583296518176, 0.9682682377875933, -1.0795861692371957E-4)),
+            Data(Vsop87AMarsData, 0.5, doubleArrayOf(-1.5432315120596511, -0.5035899853317461, 0.027202004460763252)),
+            Data(Vsop87AJupiterData, 0.5, doubleArrayOf(-2.3910546127650303, 4.664070272032234, 0.033966714165738876)),
+            Data(Vsop87ASaturnData, 0.5, doubleArrayOf(4.766239751052515, -8.7737132940204, -0.037879624876213304)),
+            Data(Vsop87AUranusData, 0.5, doubleArrayOf(-17.823171829534186, 4.071512823576951, 0.2458509882743705)),
+            Data(Vsop87ANeptuneData, 0.5, doubleArrayOf(17.397928561207056, 24.197927112419535, -0.8992271522836394))
 
     )
 

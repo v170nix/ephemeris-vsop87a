e05dbc161385375a61ff9ba43120018783f1af4a
Vitaliy Sheyanov <vit.onix@gmail.com>
2021-03-21 23:24:05 +0300

    version 0.8.7-b1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.net.URI
 import org.gradle.api.publish.maven.MavenPublication
 
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.4.31"
     id("com.github.johnrengelman.shadow") version "5.2.0"
 //    id("java")
     `maven-publish`
@@ -16,7 +16,7 @@ plugins {
 
 group = "net.arwix.astronomy2"
 val artifactID = "ephemeris-vsop87a"
-version = "0.8.7-b"
+version = "0.8.7-b1"
 
 //setProperty("targetCompatibility", JavaVersion.VERSION_1_6)
 //setProperty("sourceCompatibility", JavaVersion.VERSION_1_6)
@@ -49,7 +49,7 @@ shadowJar.apply {
 dependencies {
     implementation(kotlin("stdlib-jdk7"))
     implementation(kotlin("stdlib"))
-    implementation("net.arwix.astronomy2:astronomy-core:0.8.7-b")
+    implementation("net.arwix.astronomy2:astronomy-core:0.8.9-b1")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.2.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.2.0")
